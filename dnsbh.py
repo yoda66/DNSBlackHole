fb44d86bbb6139daa3c0a45e146146bb61798b84
Joff Thyer <jsthyer@thyer.org>
2016-07-15 12:32:27 -0400

    .

--- dnsbh.py
+++ dnsbh.py
@@ -7,34 +7,57 @@ import sys
 from datetime import datetime
 
 
-def valid_ipv4_addr(ip):
-    """
-    Returns true if an IPv4 address is valid
-    """
-    octet = r'(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])'
-    rxp = re.compile(r'^((?:%s\.){3}(?:%s))$' % (octet, octet))
-    m = rxp.match(ip)
-    if m:
-        return True
-    return False
+class DNSBH:
 
+    # dict to hold unique domains
+    DOMAINS = {}
 
-def fetchurl(url):
-    content = urllib2.urlopen(url).read()
-    print '[*] Fetched %d bytes from %s' % (len(content), url)
-    return content
+    def __init__(self, url, zonefile='db.blackhole',
+                 namedconf='named.bh.conf',
+                 ttl=3600, ip='127.0.0.1', bdir='', banner=''):
+        self.url = url
+        self.zonefile = zonefile
+        self.namedconf = namedconf
+        self.ttl = ttl
+        self.ip = ip
+        self.banner = banner
+        self.bdir = bdir
+        if self.bdir and self.bdir[-1] != '/':
+            self.bdir += '/'
 
+        if not self.valid_ipv4_addr(self.ip):
+            raise Exception('Invalid IP Address')
+        self.content = self.fetchurl()
+        self.create_zone_file()
+        self.create_named_conf()
+        return
 
-def create_zone_file(bh_zonefile, ttl=3600, ip='127.0.0.1', banner=''):
-    """
-    Creates a bind9 compatible zone file
-    that redirects queries to the specified address
-    """
-    print '[*] Created zonefile [%s], TTL:%d, IP:%s' \
-            % (bh_zonefile, ttl, ip)
-    f = open(bh_zonefile, 'w')
-    now = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S UTC')
-    f.write("""\
+    def valid_ipv4_addr(self, ip):
+        """
+        Returns true if an IPv4 address is valid
+        """
+        octet = r'(?:[0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])'
+        rxp = re.compile(r'^((?:%s\.){3}(?:%s))$' % (octet, octet))
+        m = rxp.match(ip)
+        if m:
+            return True
+        return False
+
+    def fetchurl(self):
+        content = urllib2.urlopen(self.url).read()
+        print '[*] Fetched %d bytes from %s' % (len(content), self.url)
+        return content
+
+    def create_zone_file(self):
+        """
+        Creates a bind9 compatible zone file
+        that redirects queries to the specified address
+        """
+        print '[*] Created zonefile [%s], TTL:%d, IP:%s' \
+               % (self.zonefile, self.ttl, self.ip)
+        f = open(self.zonefile, 'w')
+        now = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S UTC')
+        f.write("""\
 ;
 ; %s
 ; Auto-generated: %s
@@ -49,50 +72,49 @@ $TTL %s
 ;
 @   IN  NS  localhost.
 @   IN  A   %s
-""" % (banner, now, ttl, ip))
-    f.close()
-    return
+""" % (self.banner, now, self.ttl, self.ip))
+        f.close()
+        return
 
 
-def create_named_conf(content, bh_zonefile,
-                    named_filename, bdir='', banner=''):
-    """
-    Creates a bind9 configuration file for all
-    domains in the data passed into the function.
-    """
-    # regular expression to match a domain name
-    rxp = re.compile(
-        r'.+?(?P<domain>(?:[a-z0-9\-]+\.){1,}(?:[a-z]+))',
-        re.IGNORECASE
-    )
+    def create_named_conf(self):
+        """
+        Creates a bind9 configuration file for all
+        domains in the data passed into the function.
+        """
+        # regular expression to match a domain name
+        rxp = re.compile(
+            r'.+?(?P<domain>(?:[a-z0-9\-]+\.){1,}(?:[a-z]+))',
+            re.IGNORECASE
+        )
 
-    if bdir and bdir[-1] != '/':
-        bdir += '/'
-
-    f = open(named_filename, 'w')
-    now = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S UTC')
-    f.write("""\
+        f = open(self.namedconf, 'w')
+        now = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S UTC')
+        f.write("""\
 //
 // %s
 // Filename .......: %s
 // Auto-Generated..: %s
 //
-""" % (banner, named_filename, now))
+""" % (self.banner, self.namedconf, now))
 
-    recs = 0
-    for line in content.split('\n'):
-        if line and line[0] == '#':
-            continue
-        m = rxp.match(line)
-        if m:
-            recs += 1
-            domain = m.group('domain').lower()
-            output = 'zone "%s" { type master; file "%s%s"; };\n' \
-                % (domain, bdir, bh_zonefile)
-            f.write(output)
-    print '[*] %d domains written to file: [%s]' % (recs, named_filename)
-    f.close()
-    return
+        for line in self.content.split('\n'):
+            if line and line[0] == '#':
+                continue
+            m = rxp.match(line)
+            if m:
+                domain = m.group('domain').lower()
+                self.DOMAINS[domain] = 1
+
+        # write to file
+        for domain in sorted(self.DOMAINS.keys()):
+                output = 'zone "%s" { type master; file "%s%s"; };\n' \
+                          % (domain, self.bdir, self.zonefile)
+                f.write(output)
+        recs = len(self.DOMAINS)
+        print '[*] %d domains written to file: [%s]' % (recs, self.namedconf)
+        f.close()
+        return
 
 
 if __name__ == '__main__':
@@ -121,25 +143,14 @@ if __name__ == '__main__':
     parser.add_argument('--ttl', type=int, default=3600)
     args = parser.parse_args()
 
-    if not valid_ipv4_addr(args.ip):
-        parser.print_help()
-        print '\r\n[-] ERROR: Invalid IP address specified'
-        sys.exit()
-
-    print '[*] %s' % (banner)
     try:
-        content = fetchurl(args.url)
-        create_zone_file(
-            args.bhzonefile,
+        DNSBH(
+            args.url,
+            zonefile=args.bhzonefile,
+            namedconf=args.namedconf,
+            bdir=args.bdir,
             ttl=args.ttl,
             ip=args.ip,
-            banner=banner
-        )
-        create_named_conf(
-            content,
-            args.bhzonefile,
-            args.namedconf,
-            args.bdir,
             banner=banner
         )
     except Exception as e:
